2711cb744946371e32a332d6c4663f2260dc9e61
Jose Francisco Padinha Teixeira ( - ) <a52107@deei.fct.ualg.pt>
2017-11-02 12:27:49 +0000

    failed lab

--- register_action.php
+++ register_action.php
@@ -1,70 +1,36 @@
 <?php
- require_once "HTML/Template/IT.php";
-include 'db.php'; 
-  // Process signup submission
-  $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);  
+require_once ('HTML/Template/IT.php');
+  require_once ('db.php');
 
-  if(
-     $_POST['name']  == '' or 
-     $_POST['email'] == ''
-	 {
-   error('One or more required fields were left blank.\\n'.
-         'Please fill them in and try again.');
-	 header('Location: register.php?name = $_POST['name']&email = $_POST['name']&message="Fill blank fields"');
-	 }
-	 
-	 if(strlen($_POST['pass1'] <5)
-		 error('password must have at least 5 characters');
-	  header('Location: register.php?name = $_POST['name']&email = $_POST['name']&message=""');
-   
-  // Check for existing user with the new id
-  $query = "SELECT * FROM users WHERE email = '" .$_POST[email] ."'";
-  $result = @ mysql_query($query,$db);
-  if(!$result)
-     error('A database error occurred in processing your submission');
+  $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
+  if($db){
 
-  if(mysql_num_rows($result) > 0)
-  {
-     error('A user already exists with your chosen email.\\n'.
-           'Please try another.');
-		    header('Location: register.php?name = $_POST['name']&email = ""&message="Email already exists,try another"');
+    $name=$_GET['name'];
+    $email=$_GET['email'];
+    //$senha=substr(md5($_GET['pass1']),0,32);
+    $senha=$_GET['pass1'];
+    $senha2=$_GET['pass2'];
+    $error = 0;
+    //header("Location:register.php");
+    
+
+    $sql=mysql_query("SELECT email FROM users WHERE email='$email';");
+    if(mysql_num_rows($sql)){
+      $error = 1;
+      header("Location:register.php?error=1&name=$name&email=$email");
+    }elseif($senha!=$senha2){
+      $error = 2;
+      header("Location:register.php?error=2&name=$name&email=$email");
+    }else{
+      $senha=substr(md5($_GET['password']),0,32);
+      $data=date("Y-m-d H:i:s");
+      $query="INSERT INTO users (name,email,password_digest,created_at,updated_at) VALUES ('$name','$email','$senha','$data','data');";
+      mysql_query($query,$db);
+      header("Location:register_success.html");
+    }
+    mysql_close($db); 
   }
-  
-  $userid  = $_POST[newid];
-  substr(md5($_POST['pass1']),0,32)
-  $fullname = $_POST[name];
-  $email    = $_POST[email];
-  $present_date = date("Y-m-d H:i:s");
-
-  $sql_insert = "INSERT INTO users(uid,name,email, created_at, updated_at, password_digest) 
-                 VALUES('$userid','$fullname','$email','$present_date','$present_date','$password')";
-
-  if(!mysql_query($sql_insert,$db))
-  {
-     error('A database error occurred in processing your submission');
-	 header('Location: register.php');
+  else{
+    echo "Erro ao fazer a ligacao"; 
   }
-  // Close database
-  mysql_close($db);
-             
-  // Email the new password to the person.
-  $message = "Hello
-
-Your personal account for the Project Web Site
-has been created! 
-
-Your personal login ID and password are as
-follows:
-
-   userid: $_POST[id]
-   password: $password
-
-- Figo
-";
-
-  mail($_POST['email'],"Your Password for the Website",
-       $message, "From:Figo <figo@blabla.com>");
-  
-  header("Location: signup_success.html");      
-
 ?>
